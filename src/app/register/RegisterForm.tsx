9579547cb9027565e1328f734ed6abf584138523
AMD PC <shitga@gmail.com>
2024-02-27 00:40:13 +0700

    nothing

--- src/app/register/RegisterForm.tsx
+++ src/app/register/RegisterForm.tsx
@@ -1,12 +1,25 @@
+'use client'
+import { useState } from "react";
 import Heading from "../components/Heading/heading";
+import { FieldValues, useForm } from "react-hook-form";
+import Input from "../components/inputs/Input";
 
 
 const RegisterForm
  = () => {
+    const [isLoading, setIsLoading] = useState(false);
+    const {register,handleSubmit,formState:{errors}} =useForm<FieldValues>({
+        defaultValues:{
+            name:"",
+            email:"",
+            password:""
+        }
+    })
     return ( 
         <>
         <Heading titlle="Sign Up For E-shop" />
         <hr  className="bg-slate-300 w-full h-px"/>
+        <Input id="name" label="Name" register={register} errors={errors} required />
         </>
      );
 }
