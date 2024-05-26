afb6811a1b7ba9d4e67ea0ae63d4454eb2cb459f
AMD PC <shitga@gmail.com>
2024-05-26 16:03:23 +0700

    update suspen for useSearchParams

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 'use client';
 import { Product } from '@/lib/type/product';
 import { useSearchParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 import { getProducts } from './actions/getProducts';
 import Container from './components/nav/Container';
 import HomeBanner from './components/nav/HomeBanner';
@@ -14,7 +14,6 @@ export default function Home() {
   const params = useSearchParams();
   const category = params?.get('category');
   const searchTerm = params?.get('searchTerm');
-  const currentName = '';
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -54,32 +53,33 @@ export default function Home() {
   }, [category, searchTerm]);
 
   return (
-    <div className="p-8">
-      <Container>
-        {loading ? ( // Render loading message when loading is true
-          <div>Loading</div>
-        ) : products && products.length > 0 ? (
-          <>
-            <HomeBanner />
-            <div className="grid grid-cols-2 gap-8 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
-              {products.map((product, index) =>
-                // Render the ProductCard for the first product or if the current product's name is different from the previous product's name
-                index === 0 || product.name !== products[index - 1].name ? (
-                  <ProductCard key={product.id} data={product} />
-                ) : null
-              )}
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="p-8">
+        <Container>
+          {loading ? ( // Render loading message when loading is true
+            <div>Loading...</div>
+          ) : products && products.length > 0 ? (
+            <>
+              <HomeBanner />
+              <div className="grid grid-cols-2 gap-8 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+                {products.map((product, index) =>
+                  // Render the ProductCard for the first product or if the current product's name is different from the previous product's name
+                  index === 0 || product.name !== products[index - 1].name ? (
+                    <ProductCard key={product.id} data={product} />
+                  ) : null
+                )}
+              </div>
+            </>
+          ) : (
+            <div
+              className="flex h-[50vh] w-full items-center justify-center font-mono 
+               text-2xl md:text-3xl"
+            >
+              Oops! No product found. Click &apos;All&apos; to see all products
             </div>
-          </>
-        ) : (
-          <div
-            className="flex h-[50vh] w-full items-center justify-center font-mono 
-         text-2xl md:text-3xl"
-          >
-            Oops! No product found. Click `&apos;`All`&apos;` to see all
-            products
-          </div>
-        )}
-      </Container>
-    </div>
+          )}
+        </Container>
+      </div>
+    </Suspense>
   );
 }
